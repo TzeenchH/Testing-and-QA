15b7d6040ddf180b364685b4e0c2116b407c2e99
Evgeniy A. Vasilenko <48185066+TzeenchH@users.noreply.github.com>
2021-11-22 23:55:42 +0300

    updates

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-def calc_bonus(salary: int, level: int, perf_level: float) -> float:
-    if salary is not int or level is not int:
-        raise TypeError("Введены данные некорректного формата")
-    if salary < 70000 or 750000 < level:
-        raise ValueError("Зарплата не соответствует диапазону")
-    if level < 7 or 15 < level:
-        raise ValueError("Недопустимый уровень инженера")
-    if perf_level < 1 or 5 < perf_level:
-        raise ValueError("Неверное значение Performance Review")
-    level_bonus = 0
-    perf_bonus = 0
+import decimal
+
+"""
+   Необходимо реализовать механизм расчета премии по результатам Performance Review программиста.
+   Для предоставляемых данных:
+   - ЗП инженера - [70 000..750 000] (только целочисленные значения или преобразование ЗП в таковое)
+   - Результат квартального Performance Review [1..5]
+   - Уровень инженера - [7..17]
+   - Размер премии от квартальной ЗП:
+       - 5% если lvl < 10;
+       - 10% если 10 <= lvl < 13
+       - 15% если 13 <= lvl < 15
+       - 20% если lvl >= 15
+   > - Модификатор премии:
+   >   - 0% - если результат pref-review < 2
+   >   - 25% - если 2 <= результат pref-review < 2.5
+   >   - 50% - если 2.5 <= результат pref-review < 3
+   >   - 100% - если 3 <= результат pref-review < 3.5
+   >   - 150% - если 3.5 <= результат pref-review < 4
+   >   - 200% - если результат pref-review >= 4
+   """
+
+
+def calc_level_modifier(level: int) -> decimal:
     if level < 10:
         level_bonus = 0.05
     elif 10 <= level < 13:
@@ -17,7 +30,11 @@ def calc_bonus(salary: int, level: int, perf_level: float) -> float:
         level_bonus = 0.15
     else:
         level_bonus = 0.2
+    return level_bonus
 
+
+def calc_performance_modifier(perf_level: int):
+    perf_bonus = 0
     if 2 <= perf_level < 2.5:
         perf_bonus = 0.25
     elif 2.5 <= perf_level < 3:
@@ -28,6 +45,27 @@ def calc_bonus(salary: int, level: int, perf_level: float) -> float:
         perf_bonus = 1.5
     else:
         perf_bonus = 2
+    return perf_bonus
 
-    bonus = salary * level_bonus * perf_bonus
+
+def calc_bonus(salary: int, level: int, perf_level: decimal) -> float:
+    if salary is not int or level is not int:
+        raise TypeError("Введены данные некорректного формата")
+    if salary < 70000 or 750000 < level:
+        raise ValueError("Зарплата не соответствует диапазону")
+    if level < 7 or 15 < level:
+        raise ValueError("Недопустимый уровень инженера")
+    if perf_level < 1 or 5 < perf_level:
+        raise ValueError("Неверное значение Performance Review")
+    if salary is None or level is None or perf_level is None:
+        raise NullAgrumentException("Один из входных параметров не определён")
+    level_modifier = calc_level_modifier(level)
+    perf_modifier = calc_performance_modifier(perf_level)
+
+    bonus = salary * level_modifier * perf_modifier
     return bonus
+
+
+class NullAgrumentException(Exception):
+    def __init__(self, message):
+        self.message = message
--- test_main.py
+++ test_main.py
@@ -1,11 +1,12 @@
+import _pytest
+from allpairspy import AllPairs
 
 import main
 import pytest
-import allpairspy
 
 
 def test_all_is_ok():
-    result = main.calc_bonus(100000, 10, 4)
+    result = main.calc_bonus(100000, 10, 4.1)
     assert 20000 == result
 
 
@@ -14,13 +15,48 @@ def test_incorrect_value_value_error():
         main.calc_bonus(70000, 10, 4)
 
 
-salaries = list(range(70000, 750001, 1000))
-levels = list(range(7, 18))
-perf_results = list(x * 0.1 for x in range(10, 51))
-parameters = [
-    salaries,
-    levels,
-    perf_results,
+def test_incorrect_type_type_error():
+    with pytest.raises(TypeError):
+        main.calc_bonus("adad", 10, 5)
+
+
+null_exception_test_preset = [
+    dict(salary=None, level=10, perf_level=4),
+    dict(salary=75000, level=None, perf_level=4),
+    dict(salary=75000, level=10, perf_level=None)
 ]
 
-feed_to_test = allpairspy.AllPairs(parameters)
+
+@pytest.mark.parametrize("preset", null_exception_test_preset)
+def test_null_argument_null_argument_exception(preset):
+    with pytest.raises(main.NullAgrumentException):
+        main.calc_bonus(salary=preset.get("salary"),
+                        level=preset.get("level"),
+                        perf_level=preset.get("perf_level"))
+
+
+"""Граничные значения зарплаты"""
+preset_salary_borders = [69900, 70000, 750000, 751000]
+"""Граничные значения уровня"""
+preset_level_borders = [6, 7, 17, 18]
+"""Граничные значения оценки производительности"""
+preset_perf_level_borders = [0.9, 1, 5, 5.1]
+"""Минимальное возможное значение бонуса"""
+min_result = 0
+"""Максимальное возможное значение бонуса"""
+max_result = 300000
+
+
+# @pytest.mark.parametrize(["salary", "level", "perf_level"], [values for values in
+#                                                              AllPairs([preset_salary_borders,
+#                                                                        preset_level_borders,
+#                                                                        preset_perf_level_borders])])
+# def test_calc_bonus_borders(salary, level, perf_level):
+#     try:
+#         with pytest.raises(ValueError) as exc:
+#             result = main.calc_bonus(salary=salary, level=level, perf_level=perf_level)
+#         assert str(exc.value) in ["Зарплата не соответствует диапазону",
+#                                   "Недопустимый уровень инженера",
+#                                   "Неверное значение Performance Review"]
+#     except _pytest.outcomes.Failed:
+#         assert min_result < result < max_result
